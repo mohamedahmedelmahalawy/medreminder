505425b86bf0e2cc9a11b1501d5bda3c878790d3
AbdElRahman-SaadElDin <131001698+AbdElRahman-SaadElDin@users.noreply.github.com>
2025-08-19 19:19:58 +0300

    Test

--- src/app/(routes)/profile/layout.tsx
+++ src/app/(routes)/profile/layout.tsx
@@ -1,26 +1,22 @@
 "use client";
 
 import Sidebar from "@/app/components/Sidebar";
-import React, { useState } from "react";
+export default function ProfileLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const handleLogout = () => {
+    console.log("Logging out...");
+    alert("You Logged Out Successfully");
+  };
 
-interface ProfileLayoutProps {
-	children: React.ReactNode;
-}
-
-export default function ProfileLayout({ children }: ProfileLayoutProps) {
-	const [activeTab, setActiveTab] = useState<string>("dashboard");
-
-	const handleLogout = () => {
-		console.log("Logging out...");
-		alert("You Logged Out Successfully");
-	};
-
-	return (
-		<div className='flex min-h-[850px] bg-gray-50'>
-			{/* Sidebar */}
-			<Sidebar />
-			{/* Main Content */}
-			<div className='flex-1 flex flex-col'>{children}</div>
-		</div>
-	);
+  return (
+    <div className="flex min-h-[850px] bg-gray-50">
+      {/* Sidebar */}
+      <Sidebar />
+      {/* Main Content */}
+      <div className="flex-1 flex flex-col">{children}</div>
+    </div>
+  );
 }
