d8f3c86b41d0d6d18650039a693a7b3181eab5e9
AbdElRahman-SaadElDin <131001698+AbdElRahman-SaadElDin@users.noreply.github.com>
2025-08-26 00:31:37 +0300

    push gamed

--- src/app/(routes)/dashboard/page.tsx
+++ src/app/(routes)/dashboard/page.tsx
@@ -5,52 +5,49 @@ import CalenderENInput from "@/components/CalenderENInput";
 import FilterInput, { Filters } from "@/components/FilterInput";
 import SearchInput from "@/components/SearchInput";
 import TableOriginUI from "@/components/TableOriginUI";
-import React, { useMemo, useState } from "react";
+import React, { useState } from "react";
 import type { DateRange } from "react-day-picker";
 
-export default function page() {
-  const [dateRange, setDateRange] = useState<DateRange | undefined>();
-  const [filters, setFilters] = useState<Filters>({
-    realTime: false,
-    topChannels: false,
-    lastOrders: false,
-    totalSpent: false,
-  });
-  return (
-    <>
-      {/* bg-[#000D44]/5 */}
+export default function Page() {
+	const [dateRange, setDateRange] = useState<DateRange | undefined>();
+	const [filters, setFilters] = useState<Filters>({
+		realTime: false,
+		topChannels: false,
+		lastOrders: false,
+		totalSpent: false,
+	});
 
-      <div className="mx-auto mt-10 pb-3 w-3/4 min-h-screen">
-        <div className="mb-4 pt-10 rounded-md font-semibold text-[#000D44] text-2xl text-center">
-          <p>All Patients</p>
-        </div>
+	return (
+		<PrivateRoute requiredRole={["medical"]}>
+			<div className='mx-auto mt-10 pb-3 w-3/4 min-h-screen'>
+				<div className='mb-4 p-4 rounded-md font-semibold text-[#000D44] text-2xl text-center'>
+					<p>All Patients</p>
+				</div>
 
+				<div className='flex flex-row justify-between align-middle'>
+					<div className='w-1/4'>{/* <SearchInput /> */}</div>
 
-      <PrivateRoute requiredRole={["medical"]}>
-        <div className="mx-auto mt-10 pb-3 w-3/4 min-h-screen">
-          <div className="mb-4 p-4 rounded-md font-semibold text-[#000D44] text-2xl text-center">
-            <p>All Patients</p>
-          </div>
+					<div className='flex flex-row gap-5'>
+						{/* <CalenderENInput value={dateRange} onChange={setDateRange} /> */}
+						{/* <FilterInput
+              value={filters}
+              onApply={setFilters}
+              onClear={() =>
+                setFilters({
+                  realTime: false,
+                  topChannels: false,
+                  lastOrders: false,
+                  totalSpent: false,
+                })
+              }
+            /> */}
+					</div>
+				</div>
 
-          <div className="flex flex-row justify-between align-middle">
-            <div className="w-1/4">{/* <SearchInput /> */}</div>
-
-            <div className="flex flex-row gap-5">
-              {/* <CalenderENInput value={dateRange} onChange={setDateRange} /> */}
-              {/* <FilterInput
-							value={filters}
-							onApply={setFilters}
-							onClear={() =>
-								setFilters({ realTime: false, topChannels: false, lastOrders: false, totalSpent: false })
-							}
-						/> */}
-            </div>
-          </div>
-          <div className="mt-10">
-            <TableOriginUI filters={filters} />
-          </div>
-        </div>
-      </PrivateRoute>
-    </>
-  );
+				<div className='mt-10'>
+					<TableOriginUI filters={filters} />
+				</div>
+			</div>
+		</PrivateRoute>
+	);
 }
