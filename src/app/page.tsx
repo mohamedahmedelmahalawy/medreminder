62aca7c6a6eb6815dfbaefa70dacf52048edae7b
mizomico <aroky.me@gmail.com>
2025-08-25 07:02:53 +0300

    fix 9  mahalawy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,28 +13,21 @@ export default function Home() {
   return (
     <>
       <main>
-        <section className="relative mx-auto px-8 py-18 md:py-24 min-h-screen overflow-hidden text-white
-   flex flex-col
-    [@media(min-width:920px)]:justify-center
-    [@media(min-width:1280px)]:justify-center
-    [@media(min-width:1280px)]:py-0
-    [@media(min-width:2440px)]:justify-start
-    [@media(min-width:2440px)]:py-9
-  ">
+        <section className="relative flex flex-col [@media(min-width:1280px)]:justify-center [@media(min-width:2440px)]:justify-start [@media(min-width:920px)]:justify-center mx-auto px-8 py-18 [@media(min-width:1280px)]:py-0 [@media(min-width:2440px)]:py-9 md:py-24 min-h-screen overflow-hidden text-white">
           <BackGroundVideo />
           <div className="flex flex-col items-center gap-4 mb-4 text-center">
             <h1
               style={{ ["--n" as string]: "32ch" }}
-              className="inline-block overflow-hidden font-mono font-semibold text-[48px] md:text-[64px] align-baseline leading-tight whitespace-nowrap animate-[type-hold_23s_steps(32,end)_infinite] /* 23s total = ~3s typing + 20s hold; no caret/border */"
+              className="inline-block overflow-hidden font-mono font-semibold text-[48px] md:text-[84px] align-baseline leading-tight whitespace-nowrap animate-[type-hold_23s_steps(32,end)_infinite] /* 23s total = ~3s typing + 20s hold; no caret/border */"
             >
               Connecting Medical <br /> Staff and Patients
             </h1>
 
-            <p className="inline-block w-2/3 text-2xl animate-enter-pulse-3_5s md:1/3">
+            <p className="inline-block mb-6 w-2/3 text-2xl animate-enter-pulse-3_5s md:1/3">
               One Powerful App for Every Step of Care
             </p>
             <a href="#features">
-              <button className="inline-block bg-transparent mt-8 px-14 py-4 border-2 border-white rounded-full font-semibold text-white hover:border-blue-600 animate-enter-pulse-3_5s">
+              <button className="inline-block bg-transparent mt-8 mb-10 px-14 py-4 border-2 border-white hover:border-blue-600 rounded-full font-semibold text-white animate-enter-pulse-3_5s">
                 Learn More
               </button>
             </a>
