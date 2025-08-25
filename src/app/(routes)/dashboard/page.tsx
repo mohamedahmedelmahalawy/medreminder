426ae1ce5ae95f120f4cf8fe419e79c90ccae4f4
mizomico <aroky.me@gmail.com>
2025-08-25 04:10:39 +0300

    fix 8 loading & not-found pages  mahalawy

--- src/app/(routes)/dashboard/page.tsx
+++ src/app/(routes)/dashboard/page.tsx
@@ -8,42 +8,39 @@ import React, { useMemo, useState } from "react";
 import type { DateRange } from "react-day-picker";
 
 export default function page() {
-	const [dateRange, setDateRange] = useState<DateRange | undefined>();
-	const [filters, setFilters] = useState<Filters>({
-		realTime: false,
-		topChannels: false,
-		lastOrders: false,
-		totalSpent: false,
-	});
-	return (
-		<>
+  const [dateRange, setDateRange] = useState<DateRange | undefined>();
+  const [filters, setFilters] = useState<Filters>({
+    realTime: false,
+    topChannels: false,
+    lastOrders: false,
+    totalSpent: false,
+  });
+  return (
+    <>
+      {/* bg-[#000D44]/5 */}
+      <div className="mx-auto mt-10 pb-3 w-3/4 min-h-screen">
+        <div className="mb-4 p-4 rounded-md font-semibold text-[#000D44] text-2xl text-center">
+          <p>All Patients</p>
+        </div>
 
-			<div className="w-3/4 mx-auto mt-10 min-h-screen pb-3">
-				<div className="text-2xl font-semibold mb-4 bg-[#000D44]/5 p-4 text-[#000D44] text-center rounded-md">
-					<p>All Patients</p>
-				</div>
+        <div className="flex flex-row justify-between align-middle">
+          <div className="w-1/4">{/* <SearchInput /> */}</div>
 
-				<div className="flex flex-row justify-between align-middle">
-					<div className="w-1/4">
-						{/* <SearchInput /> */}
-					</div>
-
-					<div className="flex flex-row gap-5">
-						{/* <CalenderENInput value={dateRange} onChange={setDateRange} /> */}
-						{/* <FilterInput
+          <div className="flex flex-row gap-5">
+            {/* <CalenderENInput value={dateRange} onChange={setDateRange} /> */}
+            {/* <FilterInput
 							value={filters}
 							onApply={setFilters}
 							onClear={() =>
 								setFilters({ realTime: false, topChannels: false, lastOrders: false, totalSpent: false })
 							}
 						/> */}
-					</div>
-
-				</div>
-				<div className="mt-10 ">
-					  <TableOriginUI  filters={filters} />
-				</div>
-			</div>
-		</>
-	);
+          </div>
+        </div>
+        <div className="mt-10">
+          <TableOriginUI filters={filters} />
+        </div>
+      </div>
+    </>
+  );
 }
